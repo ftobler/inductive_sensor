dfa401102b5fd764d8f32f4d2fb8410b58ba02ba
Florin Tobler <florin.tobler@hotmail.com>
2023-12-28 01:02:52 +0100

    comments

--- stm-inductive-sensor/application/application.c
+++ stm-inductive-sensor/application/application.c
@@ -26,7 +26,6 @@ static uint32_t task_start = 0;
 
 
 static void main_task();
-static void process_data();
 static void process_data_sin();
 static void process_data_cos();
 static void process_data_final();
@@ -34,9 +33,6 @@ static void process_data_final();
 typedef struct {
 	float sin;
 	float cos;
-	float rms_sin;
-	float rms_cos;
-	float rms_tx;
 	float amplitude;
 	float angle;
 	float distance;
@@ -49,17 +45,19 @@ static volatile App_t app = {0};
 void application_init() {
 	//timer 1 is used for re-starting the measurement task and to output the result
 	//on PWM channel 1 (PA8). 1ms <=> 1cm
-	//measurement runs every 50ms
+	//measurement runs every 50ms and is also controlled through this timer
 	HAL_TIM_Base_Start_IT(&htim1);
 	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 
 	//timer17 is used to generate the TX coil signal.
 	//the PWM is always active, but set to 0 for no output
     HAL_TIM_PWM_Start(&htim17, TIM_CHANNEL_1);
-	htim17.Instance->CCR1 = 0;
-	htim17.Instance->ARR = 512;
+	htim17.Instance->CCR1 = 0; //capture compare (PWM value) / off - will be set to 50% of ARR when ON.
+	htim17.Instance->ARR = 512; //auto reload register / this sets the TX frequency
 
 	//calibrate the ADC
+	//the adc is configured with DMA and set to 2 channel scan.
+	//the actual channels are set in the measuring task.
     HAL_ADCEx_Calibration_Start(&hadc1);
 	HAL_Delay(10);
 
@@ -72,13 +70,27 @@ void appliation_loop() {
 	}
 }
 
+
+/**
+ * Do one measurement. Taskes ~40ms depending on how large the DMA buffer is.
+ * Main time is spent in calculations, this is originally made for a STM32G030 without FPU.
+ *
+ * To prevent asymmetry in measurements, the ADC DMA is started 2 times. First to capture the sine signal and the tx
+ * and then to capture the cosine and the tx. Tx is captured for a simplified lock-in amplifier. The lock-in is missing a
+ * discrete sine/cosine path because in this application the phase is fixed and only the amplitude (with sign) is needed.
+ *
+ * The ADC is set to a relatively high sampling rate. It is critical that the two measured signals do not have a big
+ * delay between them. Due to the high sampling rate, the DMA buffer is filled fast.
+ */
 void main_task() {
 	ADC_ChannelConfTypeDef sConfig = {0};
 
 
+	//// SINE CAPTURE ////
+
 	//start the timer to generate PWM for the TX coil
 	htim17.Instance->CCR1 = htim17.Instance->ARR / 2;
-	//delay very slightly so a few cycles pass
+	//delay so the DC goes away
 	volatile uint16_t i;
 	i = 2500; while (i--);
 
@@ -94,6 +106,7 @@ void main_task() {
 	sConfig.SamplingTime = ADC_SAMPLINGTIME_COMMON_1;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 		while(1);
+
 	//start the DMA transfer
 	dma_finish = 0;
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)dma_buf, DATASIZE*DATAWIDTH);
@@ -101,13 +114,16 @@ void main_task() {
 	while (dma_finish == 0);
 	//stop the PWM
 	htim17.Instance->CCR1 = 0;
+
 	//finish calculations
 	process_data_sin();
 
 
+	//// COSINE CAPTURE ////
+
 	//start the timer to generate PWM for the TX coil
 	htim17.Instance->CCR1 = htim17.Instance->ARR / 2;
-	//delay very slightly so a few cycles pass
+	//delay so the DC goes away
 	i = 2500; while (i--);
 
 	//configure ADC channels to capture
@@ -121,18 +137,20 @@ void main_task() {
 	sConfig.SamplingTime = ADC_SAMPLINGTIME_COMMON_1;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 		while(1);
+
 	//start the DMA transfer
 	dma_finish = 0;
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)dma_buf, DATASIZE*DATAWIDTH);
 	//wait for the DMA finish interrupt (flag DMA_FLAG_TC1)
 	while (dma_finish == 0);
+
 	//stop the PWM
 	htim17.Instance->CCR1 = 0;
 	//finish calculations
 	process_data_cos();
 
 
-
+	//// FINISH AND OUTPUT MEASUREMENT ////
 
 	//process all data
 	process_data_final();
@@ -146,28 +164,30 @@ void main_task() {
 	HAL_UART_Transmit_DMA(&huart1, (uint8_t*)&distance, sizeof(distance));
 }
 
+/**
+ * ISR
+ * interrupt on ADC DMA complete.
+ */
 void application_dma_complete_isr() {
 	dma_finish = 1;
 }
 
+/**
+ * ISR
+ * interrupt on timer1 complete and ready to start new measurement
+ */
 void application_timer_isr() {
 	task_start = 1;
 }
 
 
-//#define get_sin(x) (dma_buf[(x)*DATAWIDTH+2])
-//#define get_cos(x) (dma_buf[(x)*DATAWIDTH+0])
-//#define get_tx(x)  (dma_buf[(x)*DATAWIDTH+1])
-
+//macros to extract the right values from the DMA buffer
 #define get_sin(x) (dma_buf[(x)*DATAWIDTH+1])
 #define get_cos(x) (dma_buf[(x)*DATAWIDTH+1])
 #define get_tx(x)  (dma_buf[(x)*DATAWIDTH+0])
 
-//for ARR=888
-//float bias_cal_sin = 6.0f;
-//float bias_cal_cos = 5.5f;
-//float bias_cal_tx = 87.0f;
-
+//calibration to cancel out interference on the PCB by digitally mixing the TX signal in the receive paths.
+//it no metal is near the sensor strip, the app.sin and app.cos variables should hover around 0.
 //for ARR=512
 float coupling_cal_sin = 0.0f;   //to cancel out signal bleed in from TX
 float coupling_cal_cos = 0.009f; //to cancel out signal bleed in from TX
@@ -242,73 +262,3 @@ static void process_data_final() {
 }
 
 
-
-
-
-
-
-
-
-static void process_data() {
-
-	float sum_sin = 0.0f;
-	float sum_cos = 0.0f;
-
-	float avg_sin = 0.0f;
-	float avg_cos = 0.0f;
-	float avg_tx = 0.0f;
-
-	float rms_sin = 0.0f;
-	float rms_cos = 0.0f;
-	float rms_tx  = 0.0f;
-
-	const int n = DATASIZE;
-
-	for (int i = 0; i < n; i++) {
-		avg_sin += get_sin(i);
-		avg_cos += get_cos(i);
-		avg_tx  += get_tx(i);
-	}
-	avg_sin = avg_sin / n;
-	avg_cos = avg_cos / n;
-	avg_tx  = avg_tx  / n;
-
-	for (int i = 0; i < n; i++) {
-		float tx_hp  = get_tx(i)  - avg_tx;
-		float sin_hp = get_sin(i) - avg_sin + tx_hp * coupling_cal_sin;
-		float cos_hp = get_cos(i) - avg_cos + tx_hp * coupling_cal_cos;
-
-		sum_sin = sum_sin + sin_hp * tx_hp;
-		sum_cos = sum_cos + cos_hp * tx_hp;
-
-	    rms_sin += sin_hp * sin_hp;
-		rms_cos += cos_hp * cos_hp;
-		rms_tx  += tx_hp * tx_hp;
-	}
-	sum_sin = sum_sin / n / 4096;
-	sum_cos = sum_cos / n / 4096;
-	app.sin = sum_sin;
-	app.cos = sum_cos;
-
-	rms_sin = sqrtf(rms_sin / n);
-	rms_cos = sqrtf(rms_cos / n);
-	rms_tx  = sqrtf(rms_tx  / n);
-
-	app.rms_sin = rms_sin * 0.1f + app.rms_sin * 0.9f;
-	app.rms_cos = rms_cos * 0.1f + app.rms_cos * 0.9f;
-	app.rms_tx  = rms_tx  * 0.1f + app.rms_tx  * 0.9f;
-
-	float angle_old = app.angle;
-	app.angle = atan2(sum_sin, sum_cos);
-	float angle_diff = app.angle - angle_old;
-	if (angle_diff > 2.0f && app.n > 0) {
-	    app.n--;
-	}
-	if (angle_diff < -2.0f && app.n < 4) {
-		app.n++;
-	}
-	app.distance = ((app.angle  / 2.0f / 3.14159f) + app.n) * 80.0f;
-	app.distance_filter = app.distance_filter * 0.9f + app.distance * 0.1f;
-
-	app.amplitude = sqrtf(sum_sin*sum_sin + sum_cos*sum_cos);
-}
